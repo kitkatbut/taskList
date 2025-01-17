7bdc00ddbef762679b430b76c414be14a3f97026
Albaficapis <albaficapisukesu@mail.com>
2025-01-17 21:40:53 +0100

    Add form for taskList

---
+++ script.js
@@ -0,0 +1,5 @@
+taskList = [
+    {name: "Nom de la tâche", description: "Description de la tâche", estimatedDuration: 15, status: "En cours"},
+    {name: "Autre tâche", description: "Autre description", estimatedDuration: 30, status: "À faire"},
+   
+  ];
